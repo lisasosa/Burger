265ee794889d08f23c2a862eca97195be3c5c86c
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-07-12 11:10:17 -0500

    initial set up

---
+++ DB/schema.sql
@@ -0,0 +1,16 @@
+-- Create the `burgers_db`.
+--     - Switch to or use the `burgers_db`.
+--     - Create a `burgers` table with these fields:
+--         - **id**: an auto incrementing int that serves as the primary key.
+--         - **burger_name**: a string.
+--         - **devoured**: a boolean.
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured boolean DEFAULT false,
+    PRIMARY KEY (id)
+);
