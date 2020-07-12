9d0debf78574cda65a12fb719f3c86507208d456
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-07-12 18:42:06 -0500

    added selections and insert queries

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express');
-var boydyParser = require('body-parser');
-var exprhbr = require('express-handlebars');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
