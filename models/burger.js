7c7d2214846d4fa1ea9e288306143a04ba9adbd0
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-07-14 20:22:51 -0500

    added burger model

---
+++ models/burger.js
@@ -0,0 +1,31 @@
+// import `orm.js` into `burger.js`
+
+// Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+
+// Export at the end of the `burger.js` file.
+
+var orm = require("../config/orm");
+const {
+    insertOne
+} = require("../config/orm");
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll('burgers', function (res) {
+            cb(res);
+        })
+    },
+
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, function (res) {
+            cb(res);
+        })
+    },
+
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne('burgers', objColVals, condition, function (res) {
+            cb(res);
+        })
+    }
+};
+moduele.exports = burger;
